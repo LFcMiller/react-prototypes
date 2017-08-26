10827ddd22a0c5411f8da8babf32cdccd132d09c
Cody Miller <miskatonicraft@gmail.com>
2017-08-26 11:52:23 -0700

    React Intro - Cody Miller

--- src/index.js
+++ src/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+let user = {
+    name: "Cody",
+    luckyNumber : luckyNumber()
+};
+
+function luckyNumber() {
+    return Math.floor(Math.random()*1000)+1
+};
+
+function greeting(user) {
+    return <div className="container">
+        <h1>Welcome, {user.name}!</h1>
+        <h2 className="text-muted">Your lucky number is: {user.luckyNumber}</h2>
+    </div>
+};
+
 ReactDOM.render(
-    <h1 className="container">React Prototypes!</h1>,
+    greeting(user),
     document.getElementById('root')
 );
